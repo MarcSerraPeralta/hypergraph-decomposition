db8ec5890e51f0ba2241387ac306698f294a5876
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-11-10 12:35:37 +0100

    Rename package folder

--- hypergraph_decomposition/__init__.py
+++ hypergraph_decomposition/__init__.py
@@ -1,4 +1,4 @@
-from .hyperedge_decomposition import decompose_dem
+from .decomposition import decompose_dem
 from .stim_tools import from_stim_to_dem, from_dem_to_stim
 
 __all__ = ["decompose_dem", "from_stim_to_dem", "from_dem_to_stim"]
