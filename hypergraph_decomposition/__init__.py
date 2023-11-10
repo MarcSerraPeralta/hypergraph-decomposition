f1029daa8ee93fb4613c132446feb1f25132bff6
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-11-10 15:42:50 +0100

    Update README.md and benchmarks

--- hypergraph_decomposition/__init__.py
+++ hypergraph_decomposition/__init__.py
@@ -1,4 +1,5 @@
 from .decomposition import decompose_dem
 from .stim_tools import from_stim_to_dem, from_dem_to_stim
+from .detector_error_model import DEM
 
-__all__ = ["decompose_dem", "from_stim_to_dem", "from_dem_to_stim"]
+__all__ = ["decompose_dem", "from_stim_to_dem", "from_dem_to_stim", "DEM"]
